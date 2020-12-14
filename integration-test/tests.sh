7000139124c6449a8c994b1c7cd2f19b00c39850
Brian O'Halloran <brian_o_halloran@hotmail.com>
2020-12-14 18:07:34 +0000

    Amending integration tests script to change auth/circleci references to auth/token and taking out subsequent paths in inital commands to see if they work and adding in vault path-help command to help with the debugging

--- integration-test/tests.sh
+++ integration-test/tests.sh
@@ -51,6 +51,8 @@ for i in 1 2 3 4 5; do
     if (( $i == 3 )); then
         attempt_cache_expiry="attempt_cache_expiry=5s"
     fi
+    
+    docker exec vault path-help token
 
     docker exec vault vault write auth/token circleci_token=fake \
             vcs_type=github owner=johnsmith ttl=5m max_ttl=15m \
